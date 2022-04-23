60002db33c8365214a8cde767f9979afbc452e7c
Isiah Manns <isiah.michael1018@gmail.com>
2022-04-23 00:43:45 -0400

    MapView crashes depending on window setup, and mapView config (zoom)
    Happens during fetch in loadView() of TransitVC

--- GoogleMaps-Swift/GoogleMapsSwiftDemos/Swift/AppDelegate.swift
+++ GoogleMaps-Swift/GoogleMapsSwiftDemos/Swift/AppDelegate.swift
@@ -26,16 +26,51 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     GMSServices.provideAPIKey(SDKConstants.apiKey)
 
-    // let sampleListViewController = SampleListViewController()
-    let transitViewController = TransitViewController()
-    let frame = UIScreen.main.bounds
-    let window = UIWindow(frame: frame)
-    // let navigationController = UINavigationController(rootViewController: sampleListViewController)
-    // window.rootViewController = navigationController
-    window.rootViewController = transitViewController
-    window.makeKeyAndVisible()
-    self.window = window
+      // setupAppOriginal()
+      setupAppBroken()
+      // setupAppModified()
+
+
+
 
     return true
   }
+
+    private func setupAppOriginal() {
+        let sampleListViewController = SampleListViewController()
+        let frame = UIScreen.main.bounds
+        let window = UIWindow(frame: frame)
+        let navigationController = UINavigationController(rootViewController: sampleListViewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+
+        /// Depending on the mapView config and zoom level, pushing TransitViewController to navigation controller stack seems to work fine.
+        /// Sometimes it crashes, sometimes it doesn't.
+    }
+
+    private func setupAppBroken() {
+        let transitViewController = TransitViewController()
+        let frame = UIScreen.main.bounds
+        let window = UIWindow(frame: frame)
+        let navigationController = UINavigationController(rootViewController: transitViewController)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        self.window = window
+
+        /// Changing the map zoom level around makes this work. Seems to crash at 17 for current mapView config using this window setup.
+        /// https://stackoverflow.com/questions/48274324/google-map-crash-at-specific-location-and-zoom-level-15/50818477#50818477
+        /// https://issuetracker.google.com/issues/35829548
+    }
+
+    private func setupAppModified() {
+        let transitViewController = TransitViewController()
+        let frame = UIScreen.main.bounds
+        let window = UIWindow(frame: frame)
+        window.rootViewController = transitViewController
+        window.makeKeyAndVisible()
+        self.window = window
+
+        /// No problems with zoom level when setting up the window this way.
+    }
 }
